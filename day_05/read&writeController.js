ba30bf2fd02a09080f109ea5c5ab021fa77c3553
khushbu33 <khushbu3126@gmail.com>
2026-02-13 10:05:43 +0530

    controller

--- day_05/read&writeController.js
+++ day_05/read&writeController.js
@@ -15,6 +15,26 @@ const main = async () => {
 
 main();
 
+//using student.json file read write data in student1.json file,try to read data from student1.json file and print it in console. Handle error if any.
+const main1 = async () => {
+    const data = await readJsonFile("./student.json");
+    console.log(data);
+    await writeJsonFile("./student1.json", data);
+    try{
+        const newData = await readJsonFile("./student1.json");
+        console.log(newData);
+    }
+    catch(err){
+        console.log("error in reading student1.json file",err);
+    }
+};  
+main1();
+
+
+
+
+  
+
 // import { readJsonFile } from './readAndwriteServices.js';
 
 // export const readFile = async (path) => {
